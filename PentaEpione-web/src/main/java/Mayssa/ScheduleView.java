bb86a2cadbed07bdb308398e5a7dbe8c0c76fd68
mayssabouhija <mayssa.bouhija@esprit.tn>
2019-01-01 17:39:33 +0100

    V1.3
    add Beans

---
+++ PentaEpione-web/src/main/java/Mayssa/ScheduleView.java
@@ -0,0 +1,423 @@
+package Mayssa;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
+
+import service.AffectationServiceLocal;
+import service.AgentService;
+import service.AgentServiceLocal;
+import service.CongesServiceLocal;
+import service.FluxServiceLocal;
+import service.MissionServiceLocal;
+import service.StatistiquesServiceLocal;
+import model.Affectation;
+import model.Agent;
+import model.Conges;
+import model.Flux;
+import model.Mission;
+import model.Statistiques;
+
+@ManagedBean(name = "scheduleView")
+@SessionScoped
+public class ScheduleView {
+
+	private ScheduleModel eventModel;
+
+	private ScheduleEvent event = new DefaultScheduleEvent();
+
+	private ScheduleEvent eventformap;
+
+	private Flux flux;
+
+	private int idFlux;
+
+	private Affectation affectToPass;
+
+	private List<Flux> listFlux;
+
+	private List<Date> rangeDates;
+
+	private Date selectedDate;
+
+	private String dateString;
+
+	private Mission selectedMission;
+	
+	private Statistiques stat;
+	
+	private Agent selectedAgent;
+
+	private List<Mission> listMission;
+	
+	private List<Conges> listConges;
+
+	private Map<String, Affectation> map;
+
+	@EJB
+	private AffectationServiceLocal as;
+
+	@EJB
+	private MissionServiceLocal ms;
+
+	@EJB
+	private FluxServiceLocal fs;
+	
+	@EJB
+	private AgentServiceLocal ag;
+
+	@EJB
+	private StatistiquesServiceLocal ss;
+	
+	@EJB
+	private CongesServiceLocal cgs;
+	
+	private List<Agent> listAgent;
+	
+	private List<Affectation> ListAffectation;
+
+	private final String test = "test";
+
+	@PostConstruct
+	public void init() {
+		listFlux = fs.findListFlux();
+		listConges=cgs.findAllConges();
+		listAgent = new ArrayList<Agent>();
+		eventModel = new DefaultScheduleModel();
+		map = new HashMap<String, Affectation>();
+		ListAffectation = as.findListAffectation();
+		
+		for (Affectation affectation : ListAffectation) {
+			eventformap = new DefaultScheduleEvent(affectation.getFlux().getIntitule(), affectation.getDate_debut(), affectation.getDate_fin(), "background: red;");
+			eventModel.addEvent(eventformap);
+			map.put(eventformap.getId(), affectation);
+		}
+		for (Conges c : listConges) {
+			eventModel.addEvent(new DefaultScheduleEvent(c.getAgent().getNom()+" "+c.getAgent().getPrenom(),c.getDate_debut(),c.getDate_fin(), "conges"));
+		}
+	}
+
+	public Date getRandomDate(Date base) {
+		Calendar date = Calendar.getInstance();
+		date.setTime(base);
+		date.add(Calendar.DATE, ((int) (Math.random() * 30)) + 1); // set random
+																	// day of
+																	// month
+
+		return date.getTime();
+	}
+
+	public Date getInitialDate() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(calendar.get(Calendar.YEAR), Calendar.FEBRUARY, calendar.get(Calendar.DATE), 0, 0, 0);
+
+		return calendar.getTime();
+	}
+
+	public ScheduleModel getEventModel() {
+		return eventModel;
+	}
+
+	/*
+	 * private Calendar today() { Calendar calendar = Calendar.getInstance();
+	 * calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+	 * calendar.get(Calendar.DATE), 0, 0, 0);
+	 * 
+	 * return calendar; }
+	 */
+
+	public ScheduleEvent getEvent() {
+		return event;
+	}
+
+	public void setEvent(ScheduleEvent event) {
+		this.event = event;
+	}
+
+	public void addEvent(ActionEvent actionEvent) {
+		flux = findFluxById(idFlux);
+		Affectation affect;
+		Mission mission;
+		ScheduleEvent event2;
+		event2 = new DefaultScheduleEvent(flux.getIntitule(), event.getStartDate(), event.getEndDate(),"background: red;");
+		event2.setId(event.getId());
+		if (event.getId() == null) {
+			eventModel.addEvent(event2);
+			affect = new Affectation(event.getStartDate(), event.getEndDate(), flux);
+			as.addAffectation(affect);
+			map.put(event.getId(), affect);
+		}
+
+		else {
+			int id = map.get(event.getId()).getId();
+			System.out.println("id :    " + id);
+			affect = new Affectation(id, event2.getStartDate(), event2.getEndDate(), flux);
+			eventModel.updateEvent(event2);
+			as.updateAffectation(affect);
+			map.put(event.getId(), affect);
+		}
+
+		event = new DefaultScheduleEvent();
+	}
+
+	public void deleteEvent(ActionEvent actionEvent) {
+		eventModel.deleteEvent(event);
+		Affectation affect = new Affectation();
+		affect = map.get(event.getId());
+		as.deleteAffectation(affect);
+		map.remove(event.getId());
+
+	}
+
+	public void selectFlux() {
+		affectToPass = map.get(event.getId());
+		rangeDates = getDatesBetween(affectToPass.getDate_debut(), affectToPass.getDate_fin());
+	}
+
+	public static List<Date> getDatesBetween(Date startDate, Date endDate) {
+		List<Date> datesInRange = new ArrayList<>();
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(startDate);
+
+		Calendar endCalendar = new GregorianCalendar();
+		endCalendar.setTime(endDate);
+
+		while (calendar.before(endCalendar)) {
+			Date result = calendar.getTime();
+			datesInRange.add(result);
+			calendar.add(Calendar.DATE, 1);
+		}
+		return datesInRange;
+	}
+
+	public void selectSpecificMission() throws ParseException {
+		selectedDate = StringDateConverter(dateString);
+		listMission = new ArrayList<Mission>();
+		listMission = ms.findMissionbyDateAffect(selectedDate, affectToPass);
+		for (Mission m : listMission) {
+			System.out.println("mission :  " + m.getAffectation().getId());
+		}
+		listAgent=ag.findAgentNotAffected(selectedDate);
+	}
+
+	public void deleteSelectedMission() {
+		System.out.println("removed mission : " + selectedMission.getId());
+		ms.removeMission(selectedMission);
+		listMission.remove(selectedMission);
+		//createOrupdateStat(selectedAgent, test);
+		listAgent=ag.findAgentNotAffected(selectedDate);
+	}
+	
+	public void affectAgent(){
+		Mission miss=new Mission("actif",selectedDate,affectToPass,selectedAgent);
+		ms.addMission(miss);
+		listAgent.remove(selectedAgent);
+		//createOrupdateStat(selectedAgent,true);
+		listMission=ms.findMissionbyDateAffect(selectedDate, affectToPass);
+	}
+	
+	public void createOrupdateStat(Agent agent,Boolean test){
+		stat=ss.findStat(agent);
+		if (test){
+		if(stat!=null){
+			stat.setHeure_travail(stat.getHeure_travail()+8);
+			ss.updateStat(stat);
+		}
+		else {
+			stat=new Statistiques(8,0,0,0,agent);
+			ss.addStat(stat);
+		}
+		}
+		if (!test){
+			if(stat!=null){
+				stat.setHeure_travail(stat.getHeure_travail()-8);
+				ss.updateStat(stat);
+			}
+		}
+	}
+
+	/*
+	 * Getters and Setters below , plus schedule functions !! !!
+	 */
+
+	public Date StringDateConverter(String d) throws ParseException {
+		DateFormat formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.US);
+		Date date = (Date) formatter.parse(d);
+		return date;
+	}
+
+	public void onEventSelect(SelectEvent selectEvent) {
+		event = (ScheduleEvent) selectEvent.getObject();
+	}
+
+	public void onDateSelect(SelectEvent selectEvent) {
+		event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+	}
+
+	public void onEventMove(ScheduleEntryMoveEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved",
+				"Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+		addMessage(message);
+	}
+
+	public void onEventResize(ScheduleEntryResizeEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized",
+				"Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+		addMessage(message);
+	}
+
+	private void addMessage(FacesMessage message) {
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public List<Affectation> getListAffectation() {
+		return ListAffectation;
+	}
+
+	public void setListAffectation(List<Affectation> listAffectation) {
+		ListAffectation = listAffectation;
+	}
+
+	public Flux getFlux() {
+		return flux;
+	}
+
+	public void setFlux(Flux flux) {
+		this.flux = flux;
+	}
+
+	public int getIdFlux() {
+		return idFlux;
+	}
+
+	public void setIdFlux(int idFlux) {
+		this.idFlux = idFlux;
+	}
+
+	public List<Flux> getListFlux() {
+		return listFlux;
+	}
+
+	public void setListFlux(List<Flux> listFlux) {
+		this.listFlux = listFlux;
+	}
+
+	public Flux findFluxById(int id) {
+		for (Flux f : listFlux) {
+			if (f.getId() == id)
+				return f;
+		}
+		return null;
+	}
+
+	public Affectation getAffectToPass() {
+		return affectToPass;
+	}
+
+	public void setAffectToPass(Affectation affectToPass) {
+		this.affectToPass = affectToPass;
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public List<Date> getRangeDates() {
+		return rangeDates;
+	}
+
+	public void setRangeDates(List<Date> rangeDates) {
+		this.rangeDates = rangeDates;
+	}
+
+	public Date getSelectedDate() {
+		return selectedDate;
+	}
+
+	public void setSelectedDate(Date selectedDate) {
+		this.selectedDate = selectedDate;
+	}
+
+	public List<Mission> getListMission() {
+		return listMission;
+	}
+
+	public void setListMission(List<Mission> listMission) {
+		this.listMission = listMission;
+	}
+
+	public String getDateString() {
+		return dateString;
+	}
+
+	public void setDateString(String dateString) {
+		this.dateString = dateString;
+	}
+
+	public Mission getSelectedMission() {
+		return selectedMission;
+	}
+
+	public void setSelectedMission(Mission selectedMission) {
+		this.selectedMission = selectedMission;
+	}
+
+	public List<Agent> getListAgent() {
+		return listAgent;
+	}
+
+	public void setListAgent(List<Agent> listAgent) {
+		this.listAgent = listAgent;
+	}
+
+	public Agent getSelectedAgent() {
+		return selectedAgent;
+	}
+
+	public void setSelectedAgent(Agent selectedAgent) {
+		this.selectedAgent = selectedAgent;
+	}
+
+	public Statistiques getStat() {
+		return stat;
+	}
+
+	public void setStat(Statistiques stat) {
+		this.stat = stat;
+	}
+
+	public List<Conges> getListConges() {
+		return listConges;
+	}
+
+	public void setListConges(List<Conges> listConges) {
+		this.listConges = listConges;
+	}
+
+}
---
+++ PentaEpione-web/src/main/java/Mayssa/StaffBean.java
@@ -0,0 +1,226 @@
+package Mayssa;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+
+import org.primefaces.PrimeFaces;
+
+import service.AgentServiceLocal;
+import model.Agent;
+import model.Contrat;
+import model.Site;
+import model.Staff;
+
+@ManagedBean
+@ApplicationScoped
+
+public class StaffBean implements Serializable{
+	
+	private int id;
+	private String nom;
+	private String prenom;
+	private String matricule;
+	private String contrat;
+	private int nbreHeure;
+				
+					
+					
+					private static final long serialVersionUID = 1L;
+				
+					private List<Agent> agents;
+				    
+				    private List<Agent> filteredStaff;
+				     
+				    private Agent selectedStaff;
+				    
+				    @EJB
+					private AgentServiceLocal agentService;
+					private boolean formDisplay = false;
+					private List<Agent> list = new ArrayList<Agent>();
+    
+					private Agent agent = new Agent();
+
+					public boolean isFormDisplay() {
+						return formDisplay;
+					}
+
+
+					public List<Agent> displayStaff(){
+						return list = agentService.findAll();
+					}
+					
+					
+					public void buttonAction(ActionEvent actionEvent) {
+				        addMessage("effectué avec succés!!");
+				    }
+					public void addMessage(String summary) {
+				        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+				        FacesContext.getCurrentInstance().addMessage(null, message);
+				    }
+
+					public void setFormDisplay(boolean formDisplay) {
+						this.formDisplay = formDisplay;
+					}
+					
+					
+	
+	
+					public StaffBean() {
+						super();
+					}
+
+					
+					
+					public String Save(){
+						agentService.AjoutAgent(agent);
+				        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("effectué avec succés!!"));
+						return "/faces/staff?faces-redirect=true";
+					}
+					
+					
+					public void deleteAgent() {
+				        agentService.DeleteAgent(selectedStaff);
+				        selectedStaff = null;
+				    }
+
+					
+					public void reset() {
+				        PrimeFaces.current().resetInputs("form2:panel2");
+				    }
+
+
+					public int getId() {
+						return id;
+					}
+
+
+					public void setId(int id) {
+						this.id = id;
+					}
+
+
+					public String getNom() {
+						return nom;
+					}
+
+
+					public void setNom(String nom) {
+						this.nom = nom;
+					}
+
+
+					public String getPrenom() {
+						return prenom;
+					}
+
+
+					public void setPrenom(String prenom) {
+						this.prenom = prenom;
+					}
+
+
+					public String getMatricule() {
+						return matricule;
+					}
+
+
+					public void setMatricule(String matricule) {
+						this.matricule = matricule;
+					}
+
+
+					public String getContrat() {
+						return contrat;
+					}
+
+
+					public void setContrat(String contrat) {
+						this.contrat = contrat;
+					}
+
+
+					public int getNbreHeure() {
+						return nbreHeure;
+					}
+
+
+					public void setNbreHeure(int nbreHeure) {
+						this.nbreHeure = nbreHeure;
+					}
+
+
+					public List<Agent> getAgents() {
+						return agents;
+					}
+
+
+					public void setAgents(List<Agent> agents) {
+						this.agents = agents;
+					}
+
+
+					public List<Agent> getFilteredStaff() {
+						return filteredStaff;
+					}
+
+
+					public void setFilteredStaff(List<Agent> filteredStaff) {
+						this.filteredStaff = filteredStaff;
+					}
+
+
+					public Agent getSelectedStaff() {
+						return selectedStaff;
+					}
+
+
+					public void setSelectedStaff(Agent selectedStaff) {
+						this.selectedStaff = selectedStaff;
+					}
+
+
+					public AgentServiceLocal getAgentService() {
+						return agentService;
+					}
+
+
+					public void setAgentService(AgentServiceLocal agentService) {
+						this.agentService = agentService;
+					}
+
+
+					public List<Agent> getList() {
+						return list;
+					}
+
+
+					public void setList(List<Agent> list) {
+						this.list = list;
+					}
+
+
+					public Agent getAgent() {
+						return agent;
+					}
+
+
+					public void setAgent(Agent agent) {
+						this.agent = agent;
+					}
+
+
+					
+
+
+					
+					
+}
